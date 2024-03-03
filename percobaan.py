39c593f5a68fa0e26624b09f6476e8c7069e0aa2
Farhan Aziz <farhankazai1@gmail.com>
2024-03-03 10:23:51 +0700

    Remove percobaan.py

--- percobaan.py
+++ percobaan.py
@@ -1,12 +1,31 @@
 from git import Repo
 import os
 
-repo = Repo(os.getcwd())
+# Tentukan kredensial GitLab
+username = "farhanaazziizz"
+access_token = "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"
 
-# Ambil URL remote GitLab
+# Dapatkan jalur direktori kerja saat ini
+current_directory = os.getcwd()
+
+# Inisialisasi repositori
+repo = Repo(current_directory)
+
+# Dapatkan URL remote GitLab
 remote_url = repo.remotes.origin.url
 
+# Modifikasi URL remote untuk menambahkan kredensial
+if username and access_token:
+    modified_remote_url = f"https://{username}:{access_token}@{remote_url.split('://')[1]}"
+else:
+    modified_remote_url = remote_url
+
 # Lakukan pull dari remote GitLab
-repo.remotes.origin.pull()
+file_path = os.path.join('percobaan.py')
+repo.git.add(file_path)
+repo.git.commit('-m', f"Remove percobaan.py ")
+origin = repo.remote(name='origin')
+origin.push('main')
+
 
 print("Pull selesai")
